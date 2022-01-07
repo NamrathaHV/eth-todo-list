f3efbc23529d24017674ced7260e61fdd519c567
NAMRATHA.H.V <1rn18cs067.namratha.h.v@gmail.com>
2022-01-07 10:24:32 +0530

    1. Project Setup

---
+++ contracts/TodoList.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract TodoList {
+  uint public taskCount = 0;
+
+  /*struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }*/
+}
